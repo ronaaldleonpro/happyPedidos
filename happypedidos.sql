4b4d3323b164e319e872e9b79eed3c666e1c1468
ronaaldleonpro <ronaldleonpro@gmail.com>
2024-10-04 19:28:46 -0600

    Adicion de BDD de la aplicacion de happyPedidos

---
+++ happypedidos.sql
@@ -0,0 +1,59 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 05-10-2024 a las 03:11:49
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `happypedidos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dishes`
+--
+-- Error leyendo la estructura de la tabla happypedidos.dishes: #1932 - Table &#039;happypedidos.dishes&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla happypedidos.dishes: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `happypedidos`.`dishes`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `orders`
+--
+-- Error leyendo la estructura de la tabla happypedidos.orders: #1932 - Table &#039;happypedidos.orders&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla happypedidos.orders: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `happypedidos`.`orders`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `order_dishes`
+--
+-- Error leyendo la estructura de la tabla happypedidos.order_dishes: #1932 - Table &#039;happypedidos.order_dishes&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla happypedidos.order_dishes: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `happypedidos`.`order_dishes`&#039; en la linea 1
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+-- Error leyendo la estructura de la tabla happypedidos.users: #1932 - Table &#039;happypedidos.users&#039; doesn&#039;t exist in engine
+-- Error leyendo datos de la tabla happypedidos.users: #1064 - Algo está equivocado en su sintax cerca &#039;FROM `happypedidos`.`users`&#039; en la linea 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
